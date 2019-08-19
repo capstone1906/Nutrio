28b687d9784bf64c913055b04739a3450f5e9627
jgee <jacobgee87@gmail.com>
2019-08-19 15:19:54 -0500

    login persistence and logout

--- server/api/index.js
+++ server/api/index.js
@@ -1,16 +1,13 @@
-const router = require('express').Router()
-module.exports = router
+const router = require('express').Router();
+module.exports = router;
 
-router.use('/food', require('./food'))
-router.use('/meals', require('./meals'))
-router.use('/mealFoodItems', require('./mealFoodItems'))
-router.use('/user', require('./user'))
-
-
+router.use('/food', require('./food'));
+router.use('/meals', require('./meals'));
+router.use('/mealFoodItems', require('./mealFoodItems'));
+router.use('/users', require('./users'));
 
 router.use((req, res, next) => {
-    const error = new Error('Not Found')
-    error.status = 404
-    next(error)
-  })
-  
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
--- server/index.js
+++ server/index.js
@@ -1,12 +1,11 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const db = require("./db/postgres/db");
+const db = require('./db/postgres/db');
 module.exports = app;
 
-
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use("/api", require("./api"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', require('./api'));
 
 const server = app.listen(3000, () =>
   console.log(`Mixing it up on port ${3000}`)
