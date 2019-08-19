b1e417cb41eadcdc1016ac5f50be59be90e2f53d
edgarc0998 <edgarc0998@gmail.com>
2019-08-19 13:44:38 -0500

    adding food

--- screens/FoodSearchItem.js
+++ screens/FoodSearchItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-import {connect} from 'react-redux'
+import { connect } from "react-redux";
 import { Input, Button, Divider } from "react-native-elements";
 
 import {
@@ -8,10 +8,11 @@ import {
   StyleSheet,
   Text,
   View,
-  TouchableOpacity
+  TouchableOpacity,
+  Image
 } from "react-native";
 import { VictoryPie, VictoryChart, VictoryTheme } from "victory-native";
-import {postFood} from '../components/store/meals'
+import { postFood } from "../components/store/meals";
 
 const FoodTimeHeader = props => {
   return (
@@ -41,72 +42,88 @@ class FoodSearchItem extends React.Component {
       showData: false,
       isOpen: false
     };
-    this.postFood = this.postFood.bind(this)
+    this.postFood = this.postFood.bind(this);
   }
 
   postFood() {
-    var mealId = this.props.navigation.getParam("mealId")
+    var mealId = this.props.navigation.getParam("mealId");
     var newFood = {
-      name: this.state.foodInfo.name,
-      calories: this.state.foodInfo.nutrients[0].value,
-      fat: this.state.foodInfo.nutrients[2].value,
-      protein: this.state.foodInfo.nutrients[1].value,
-      carbohydrates: this.state.foodInfo.nutrients[3].value,
-      ndbno: this.state.foodInfo.ndbno      
-    }
+      food_name: this.state.foodInfo.food_name,
+      calories: this.state.foodInfo.nf_calories,
+      fat: this.state.foodInfo.nf_total_fat,
+      protein: this.state.foodInfo.nf_protein,
+      carbohydrates: this.state.foodInfo.nf_total_carbohydrate,
+      weight: this.state.foodInfo.serving_weight_grams,
+      servingSize: this.state.foodInfo.serving_unit
+    };
 
-    this.props.postFood(newFood, mealId)
-
+    this.props.postFood(newFood, mealId);
   }
 
   async componentDidMount() {
     var food = this.props.navigation.getParam("food", "hi");
+    // console.log("name is", food.food_name);
 
-    var data;
-    const res = await axios.get(
-      `https://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=VfLLSxw4Odcu042mZ1dySCS2hLJULj5zkhtx1lLy&nutrients=205&nutrients=204&nutrients=208&nutrients=203&ndbno=${
-        food.ndbno
-      }`
+    const res = await axios.post(
+      `https://trackapi.nutritionix.com/v2/natural/nutrients`,
+      {
+        query: `${food.food_name}`
+      },
+      {
+        headers: {
+          "x-app-id": "fa4f9042",
+          "x-app-key": "997023a117b76d83e33a7ae290a6b5ba",
+          "x-remote-user-id": "0"
+        }
+      }
     );
-    data = res.data.report.foods[0];
-    data.name = food.name;
-    data.nutrients[0].value = Math.ceil(data.nutrients[0].value);
-    data.nutrients[1].value = Math.ceil(data.nutrients[1].value);
-    data.nutrients[2].value = Math.ceil(data.nutrients[2].value);
-    data.nutrients[3].value = Math.ceil(data.nutrients[3].value);
+
+    var food = res.data.foods[0];
+
+    food.nf_protein = Math.ceil(food.nf_protein);
+    food.nf_calories = Math.ceil(food.nf_calories);
+    food.nf_total_fat = Math.ceil(food.nf_total_fat);
+    food.nf_total_carbohydrate = Math.ceil(food.nf_total_carbohydrate);
+    food.serving_weight_grams = Math.ceil(food.serving_weight_grams);
+
+    console.log("results", food);
 
     this.setState({
-      foodInfo: data,
+      foodInfo: food,
       showData: !this.state.showData
     });
   }
 
   render() {
-
-
+    // console.log('state is,', this.state)
     return (
       <View>
-        {this.state.foodInfo.nutrients ? (
+        {this.state.foodInfo.nf_calories ? (
           <View style={styles.foodItemInfo}>
             <View>
-              <Text style={styles.info}>{this.state.foodInfo.name}</Text>
+              <Text style={styles.info}>{this.state.foodInfo.food_name}</Text>
               <Divider style={{ backgroundColor: "blue" }} />
             </View>
+
+            <Image
+              style={{ width: 150, height: 150 }}
+              source={{ uri: `${this.state.foodInfo.photo.thumb}` }}
+            />
 
             <View>
               <FoodTimeHeader />
               <View style={styles.FoodTimeHeader}>
                 <View style={{ flex: 1 }}>
-                  <Text>{this.state.foodInfo.nutrients[0].value}</Text>
+                  <Text>{this.state.foodInfo.nf_calories}</Text>
                 </View>
                 <View style={{ flex: 1 }}>
-                  <Text>{this.state.foodInfo.nutrients[3].value}</Text>
+                  <Text>{this.state.foodInfo.nf_total_carbohydrate}</Text>
                 </View>
                 <View style={{ flex: 1 }}>
-                  <Text>{this.state.foodInfo.nutrients[2].value}</Text>
+                  <Text>{this.state.foodInfo.nf_total_fat}</Text>
                 </View>
                 <View style={{ flex: 1 }}>
-                  <Text>{this.state.foodInfo.nutrients[1].value}</Text>
+                  <Text>{this.state.foodInfo.nf_protein}</Text>
                 </View>
               </View>
               <Divider style={{ backgroundColor: "blue" }} />
@@ -119,8 +136,23 @@ class FoodSearchItem extends React.Component {
 
               <View>
                 <Text style={styles.info} style={styles.value}>
-                  {this.state.foodInfo.measure}
+                  {this.state.foodInfo.serving_unit}
                 </Text>
+              </View>
+
+              <Divider style={{ backgroundColor: "blue" }} />
+            </View>
+
+            <View style={styles.servingSize}>
+              <View style={styles.size}>
+                <Text style={styles.info}>Number of servings:</Text>
+              </View>
+
+              <View>
+                {/* <Text style={styles.info} style={styles.value}>
+                  {this.state.foodInfo.serving_unit}
+                </Text> */}
+                <Input />
               </View>
 
               <Divider style={{ backgroundColor: "blue" }} />
@@ -134,29 +166,30 @@ class FoodSearchItem extends React.Component {
               </View>
               <View>
                 <Text style={styles.info} style={styles.value}>
-                  {this.state.foodInfo.weight}
+                  {this.state.foodInfo.serving_weight_grams} g
                 </Text>
               </View>
               <Divider style={{ backgroundColor: "blue" }} />
             </View>
+
             <Divider style={{ backgroundColor: "blue" }} />
 
-            <Button title='add' onPress={() => this.postFood()}/>
+            <Button title="add" onPress={() => this.postFood()} />
 
             <VictoryPie
               colorScale={["crimson", "limegreen", "navy"]}
               data={[
                 {
-                  x: `Fat: ${this.state.foodInfo.nutrients[2].value}g`,
-                  y: this.state.foodInfo.nutrients[2].value
+                  x: `Fat: ${this.state.foodInfo.nf_total_fat}g`,
+                  y: this.state.foodInfo.nf_total_fat
                 },
                 {
-                  x: `Carbs: ${this.state.foodInfo.nutrients[3].value}g`,
-                  y: this.state.foodInfo.nutrients[3].value
+                  x: `Carbs: ${this.state.foodInfo.nf_total_carbohydrate}g`,
+                  y: this.state.foodInfo.nf_total_carbohydrate
                 },
                 {
-                  x: `Protein: ${this.state.foodInfo.nutrients[1].value}g`,
-                  y: this.state.foodInfo.nutrients[1].value
+                  x: `Protein: ${this.state.foodInfo.nf_protein}g`,
+                  y: this.state.foodInfo.nf_protein
                 }
               ]}
               labelRadius={87}
@@ -188,7 +221,7 @@ const styles = StyleSheet.create({
   servingSize: {
     flexDirection: "row",
     justifyContent: "space-evenly",
-    width: '100%',
+    width: "100%",
     maxWidth: 300
   },
   info: {
@@ -216,16 +249,18 @@ FoodSearchItem.navigationOptions = {
 
 const mapState = state => {
   return {
-      meals: state.meals
-  }
-}
+    meals: state.meals
+  };
+};
 
 const mapDispatch = dispatch => {
   return {
-      // getMeals: () => dispatch(getMealsThunk()),
-      postFood: (food, mealId) => dispatch(postFood(food, mealId))
+    // getMeals: () => dispatch(getMealsThunk()),
+    postFood: (food, mealId) => dispatch(postFood(food, mealId))
+  };
+};
 
-  }
-}
-
-export default connect(mapState, mapDispatch)(FoodSearchItem)
+export default connect(
+  mapState,
+  mapDispatch
+)(FoodSearchItem);
