839814fced553e2f974e0e2f547054d02b703728
jgee <jacobgee87@gmail.com>
2019-08-20 11:35:34 -0500

    seed

--- server/db/postgres/models/mealFoodItems.js
+++ server/db/postgres/models/mealFoodItems.js
@@ -12,16 +12,14 @@ const MealFoodItems = db.define('mealFoodItems', {
   },
   calories: {
     type: Sequelize.INTEGER,
-    // validate: {
-    //   notEmpty: true,
-    // },
   },
   quantity: {
     type: Sequelize.INTEGER,
-    defaultValue: 1,
-    // validate: {
-    //   notEmpty: true,
-    // },
+    defaultValue: 0,
+  },
+  grams: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0,
   },
 });
 
@@ -30,10 +28,16 @@ module.exports = MealFoodItems;
 MealFoodItems.afterSave(async mealFoodItem => {
   const meal = await Meals.findByPk(mealFoodItem.mealId);
   const foodItem = await FoodItems.findByPk(mealFoodItem.foodItemId);
-  const newCals = meal.totalCalories + foodItem.calories;
-  const newCarbs = meal.totalCarbs + foodItem.carbohydrates;
-  const newProtein = meal.totalProtein + foodItem.protein;
-  const newFat = meal.totalFat + foodItem.fat;
+  let unit = 0;
+  if (mealFoodItem.quantity > 0) {
+    unit = mealFoodItem.quantity;
+  } else {
+    unit = mealFoodItem.grams / foodItem.weight;
+  }
+  const newCals = meal.totalCalories + foodItem.calories * unit;
+  const newCarbs = meal.totalCarbs + foodItem.carbohydrates * unit;
+  const newProtein = meal.totalProtein + foodItem.protein * unit;
+  const newFat = meal.totalFat + foodItem.fat * unit;
   await meal.update(
     {
       totalCalories: newCals,
@@ -46,6 +50,17 @@ MealFoodItems.afterSave(async mealFoodItem => {
 
   const newMealItem = await createMealFoodItems(mealFoodItem);
   return newMealItem;
+});
+
+MealFoodItems.beforeCreate(async mealFoodItem => {
+  const foodItem = await FoodItems.findByPk(mealFoodItem.foodItemId);
+  let unit = 0;
+  if (mealFoodItem.quantity > 0) {
+    unit = mealFoodItem.quantity;
+  } else {
+    unit = mealFoodItem.grams / foodItem.weight;
+  }
+  mealFoodItem.calories = unit * foodItem.calories;
 });
 
 MealFoodItems.beforeDestroy(async mealFoodItem => {
