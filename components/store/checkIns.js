4680f6c9bd40355f11d7429401cbbd253efa9386
edgarc0998 <edgarc0998@gmail.com>
2019-08-21 14:28:59 -0500

    exercise

---
+++ components/store/checkIns.js
@@ -0,0 +1,54 @@
+import axios from "axios";
+import { ngrok } from "../../secret";
+
+/**
+ * ACTION TYPES
+ */
+const GET_CHECKINS = "GET_CHECKINS";
+
+/**
+ * INITIAL STATE
+ */
+const checkIns = {};
+
+/**
+ * ACTION CREATORS
+ */
+const getCheckIns = checkIns => ({ type: GET_CHECKINS, checkIns });
+
+/**
+ * THUNK CREATORS
+ */
+
+export const getCheckInsThunk = () => async dispatch => {
+  try {
+    var res = await axios.get(`${ngrok}/api/checkIns/`);
+    dispatch(getCheckIns(res.data));
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const updateCheckIn = (id, checkIn) => async dispatch => {
+  try {
+    var res2 = await axios.put(`${ngrok}/api/checkIns/${id}`, checkIn);
+
+    var res = await axios.get(`${ngrok}/api/checkIns/`);
+    dispatch(getCheckIns(res.data));
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+/**
+ * REDUCER
+ */
+export default function(state = checkIns, action) {
+  switch (action.type) {
+    case GET_CHECKINS:
+      return action.checkIns;
+
+    default:
+      return state;
+  }
+}
